b2caa4417a06cdc752087bd4328200691f8c576b
unknown <andiharunlrasyid14@gmail.com>
2024-05-10 18:09:33 +0700

    percobaan kedua semoga di terima

---
+++ script.js
@@ -0,0 +1,5 @@
+function showAlert() {
+  alert(
+    "haloo, nama saya andi harun al rasyid saya berasal dari kota kendari provinsi sulawesi tenggara sekarang saya sedang menempuh kuliah di universitas muhammadiyah yogyakata fakultas teknik prodi S1 Teknologi informasi dan saya baru putus dari hubungan 4 tahun, SEKIAN!!!!!!"
+  );
+}
